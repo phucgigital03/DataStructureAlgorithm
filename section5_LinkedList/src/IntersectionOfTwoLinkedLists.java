2096904e68ef2b19ca9460dcd4964d00d5eadc3d
phucnguyen <phucnguyendigital2003@gmail.com>
2025-10-31 15:11:11 +0700

    intersection and merge with linked list

--- section5_LinkedList/src/IntersectionOfTwoLinkedLists.java
+++ section5_LinkedList/src/IntersectionOfTwoLinkedLists.java
@@ -1,2 +1,84 @@
+import java.util.Arrays;
+
 public class IntersectionOfTwoLinkedLists {
+    public static void main(String[] args) {
+        System.out.println("Intersection of two linked lists:");
+        Node headA = null;
+        Node headB = null;
+
+        Node common1 = new Node(2);
+        Node common2 = new Node(3);
+        Node common3 = new Node(4);
+        Node common4 = new Node(5);
+
+        headA = new Node(1);
+        headA.setNext(common1);
+        common1.setNext(common2);
+
+        headB = new Node(4);
+        headB.setNext(common3);
+        common3.setNext(common4);
+        common4.setNext(common1);
+        common1.setNext(common2);
+
+
+//        printList(headA);
+//        printList(headB);
+        Node intersectionNode = getIntersection(headA, headB);
+        System.out.println("intersection node: " + intersectionNode.getData());
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.println("data node: " +  current.getData());
+            current = current.getNext();
+        }
+    }
+
+    public static Node getIntersection(Node headA, Node headB) {
+        int m = findLength(headA);
+        int n = findLength(headB);
+
+        Node fp = headA;
+        Node sp = headB;
+        if(m < n){
+            int skip = n-m;
+            for(int i = 0; i < skip; i++){
+                sp = sp.getNext();
+            }
+        }else if(m > n){
+            int skip = m-n;
+            for(int i = 0; i < skip; i++){
+                fp = fp.getNext();
+            }
+        }
+
+        return findSameNode(fp, sp);
+    }
+
+    private static int findLength(Node head) {
+        Node current = head;
+        int length = 0;
+        while (current != null) {
+            length++;
+            current = current.getNext();
+        }
+        return length;
+    }
+
+//    fp -> first pointer
+//    sp -> second pointer
+    private static Node findSameNode(Node fp, Node sp) {
+        while (fp != null && sp != null) {
+            if(fp == sp){
+                return fp;
+            }else{
+                fp = fp.getNext();
+                sp = sp.getNext();
+            }
+        }
+        return null;
+    }
+
 }
--- section5_LinkedList/src/MergeTwoSortedLists.java
+++ section5_LinkedList/src/MergeTwoSortedLists.java
@@ -1,2 +1,89 @@
+import java.util.LinkedList;
+
 public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        System.out.println("Merge Two Lists:");
+//        LinkedList<Integer> ll = new LinkedList<>();
+//        ll.add(2);
+//        ll.add(4);
+//        ll.add(5);
+//        LinkedList<Integer> ll2 = new LinkedList<>();
+//        ll2.add(3);
+//        ll2.add(6);
+//        ll2.add(10);
+//        ll2.add(12);
+
+        Node ll = null;
+        ll = NodeCRUD.insertNodeAtFront(ll, 9);
+        ll = NodeCRUD.insertNodeAtFront(ll, 5);
+        ll = NodeCRUD.insertNodeAtFront(ll, 2);
+
+        Node ll2 = null;
+        ll2 = NodeCRUD.insertNodeAtFront(ll2, 100);
+        ll2 = NodeCRUD.insertNodeAtFront(ll2, 10);
+        ll2 = NodeCRUD.insertNodeAtFront(ll2, 9);
+        ll2 = NodeCRUD.insertNodeAtFront(ll2, 3);
+        ll2 = NodeCRUD.insertNodeAtFront(ll2, 2);
+
+        Node merged = mergeTwoLists(ll,ll2);
+        NodeCRUD.printList(merged);
+    }
+
+//    1. loop both headA and headB
+//    2. if both are null, and then break
+//    3. if either of them is null, copy other blindly
+//    4. if both are not null, compare them
+//          - node 1 <= node 2, copy node 1 into output list
+//          - node 1 > node 2, copy node 2 into output list
+//          - node 1 == node 2, copy either of them
+    private static Node mergeTwoLists(Node headA, Node headB) {
+        Node out = null;
+        Node tail = null;
+        while(!(headA == null && headB == null)){
+            if(headA == null){
+                if(out == null){
+                    out = headB;
+                    tail = headB;
+                }else{
+                    tail.setNext(headB);
+                    tail = headB;
+                }
+                headB = headB.getNext();
+            }else if(headB == null){
+                if(out == null){
+                    out = headA;
+                    tail = headA;
+                }else{
+                    tail.setNext(headA);
+                    tail = headA;
+                }
+                headA = headA.getNext();
+            }else{
+                if(headA.getData() <= headB.getData()){
+                    if(out == null){
+                        out = headA;
+                        tail = headA;
+                    }else{
+                        tail.setNext(headA);
+                        tail = headA;
+                    }
+                    headA = headA.getNext();
+                }else if(headA.getData() > headB.getData()){
+                    if(out == null){
+                        out = headB;
+                        tail = headB;
+                    }else{
+                        tail.setNext(headB);
+                        tail = headB;
+                    }
+                    headB = headB.getNext();
+                }
+            }
+
+        }
+
+        return out;
+    }
+
+
 }
